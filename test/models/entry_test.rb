be4bca287635e4e7e37f248520d4b2533418a43a
seankzw <56796695+seankzw@users.noreply.github.com>
2022-12-12 15:21:38 +0800

    Done

---
+++ test/models/entry_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class EntryTest < ActiveSupport::TestCase
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10, calories: 105)
+    assert entry.save
+  end
+end
+
